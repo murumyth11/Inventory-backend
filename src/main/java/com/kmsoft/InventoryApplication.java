13147a861fdfe6cc881667bf8817c93fb67d0464
murugan <murumyth11@gmail.com>
2022-05-14 14:25:11 +0530

    updates

--- src/main/java/com/kmsoft/InventoryApplication.java
+++ src/main/java/com/kmsoft/InventoryApplication.java
@@ -25,7 +25,7 @@ public class InventoryApplication extends SpringBootServletInitializer {
 	public static void main(String[] args) {
 		SpringApplication.run(InventoryApplication.class, args);
 	System.out.println(System.currentTimeMillis());
-		
+		//hai this is main
 		
 	} 
 	
